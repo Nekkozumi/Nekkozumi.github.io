5b41601fa3a284ae1dd0172ec868b33f0c01d15a
Nekkozumi <trih17012004@gmail.com>
2026-06-07 04:54:18 +0700

    Fix base url for AdMob root

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ export default defineConfig({
   // Thay đổi base thành tên repository GitHub của bạn.
   // Ví dụ: Nếu link github là github.com/nekkozumi/TheThing thì base: '/TheThing/'
   // Nếu bạn dùng custom domain hoặc dạng nekkozumi.github.io thì base: '/'
-  base: '/TheThing/',
+  base: '/',
 });
